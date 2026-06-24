380d2510e51372a4c96be12b6faf6a012b668f44
Icekech <142043949+Artemka2100@users.noreply.github.com>
2026-06-24 20:25:34 +0300

    Register entity, attributes and events

--- src/main/java/com/verte/Verte.java
+++ src/main/java/com/verte/Verte.java
@@ -1,10 +1,15 @@
 package com.verte;
 
 import com.mojang.logging.LogUtils;
+import com.verte.entity.ModEntities;
+import com.verte.entity.VerteEntity;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 @Mod(Verte.MOD_ID)
@@ -13,8 +18,17 @@ public class Verte {
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public Verte() {
+        IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+        ModEntities.ENTITY_TYPES.register(modBus);
+        modBus.addListener(this::registerAttributes);
+
         MinecraftForge.EVENT_BUS.register(new VerteCommand());
+        MinecraftForge.EVENT_BUS.register(new VerteEvents());
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, VerteConfig.SPEC);
         LOGGER.info("Verte awakens...");
+    }
+
+    private void registerAttributes(EntityAttributeCreationEvent event) {
+        event.put(ModEntities.VERTE.get(), VerteEntity.createAttributes().build());
     }
 }
