5dfc9e69eca1ee5e823475b441f6ed7958d88870
Icekech <142043949+Artemka2100@users.noreply.github.com>
2026-06-26 00:13:43 +0300

    Remove Verte chat GUI: drop the V keybind registration

--- src/main/java/com/verte/client/VerteClient.java
+++ src/main/java/com/verte/client/VerteClient.java
@@ -4,7 +4,6 @@ import com.verte.Verte;
 import com.verte.entity.ModEntities;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
-import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -19,10 +18,5 @@ public class VerteClient {
     @SubscribeEvent
     public static void registerLayers(EntityRenderersEvent.RegisterLayerDefinitions event) {
         event.registerLayerDefinition(VerteModel.LAYER, VerteModel::createBodyLayer);
-    }
-
-    @SubscribeEvent
-    public static void registerKeys(RegisterKeyMappingsEvent event) {
-        event.register(VerteKeybinds.TALK);
     }
 }
